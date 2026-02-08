4a2027da7394d1fe50c0823380b9381db15f464e
NikhilVaghela0716 <vaghelanikhil0716@gmail.com>
2026-02-08 14:43:13 +0530

    Update NikhilVaghela01.sh

--- Cloud Run Canary Deployments/NikhilVaghela01.sh
+++ Cloud Run Canary Deployments/NikhilVaghela01.sh
@@ -1,29 +1,51 @@
 #!/bin/bash
 
-export PROJECT_ID=$(gcloud config get-value project)
-export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
-export REGION=
-gcloud config set compute/region $REGION
 
-gcloud services enable \
-cloudresourcemanager.googleapis.com \
-container.googleapis.com \
-cloudbuild.googleapis.com \
-containerregistry.googleapis.com \
-run.googleapis.com \
-secretmanager.googleapis.com
+gcloud builds repositories create cloudrun-progression \
+     --remote-uri="https://github.com/${GITHUB_USERNAME}/cloudrun-progression.git" \
+     --connection="cloud-build-connection" --region=$REGION
 
 
-gcloud projects add-iam-policy-binding $PROJECT_ID \
---member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-cloudbuild.iam.gserviceaccount.com \
---role=roles/secretmanager.admin
+gcloud builds triggers create github --name="branch" \
+   --repository=projects/$PROJECT_ID/locations/$REGION/connections/cloud-build-connection/repositories/cloudrun-progression \
+   --build-config='branch-cloudbuild.yaml' \
+   --service-account=projects/$PROJECT_ID/serviceAccounts/$PROJECT_NUMBER-compute@developer.gserviceaccount.com \
+   --region=$REGION \
+   --branch-pattern='[^(?!.*master)].*'
 
 
-curl -sS https://webi.sh/gh | sh
-gh auth login
-gh api user -q ".login"
-GITHUB_USERNAME=$(gh api user -q ".login")
-git config --global user.name "${GITHUB_USERNAME}"
-git config --global user.email "${USER_EMAIL}"
-echo ${GITHUB_USERNAME}
-echo ${USER_EMAIL}
+git checkout -b new-feature-1
+
+sed -i "s/v1.0/v1.1/g" app.py
+
+git add . && git commit -m "updated" && git push gcp new-feature-1
+
+BRANCH_URL=$(gcloud run services describe hello-cloudrun --platform managed --region $REGION --format=json | jq --raw-output ".status.traffic[] | select (.tag==\"new-feature-1\")|.url")
+echo $BRANCH_URL
+
+gcloud builds triggers create github --name="master" \
+   --repository=projects/$PROJECT_ID/locations/$REGION/connections/cloud-build-connection/repositories/cloudrun-progression \
+   --build-config='master-cloudbuild.yaml' \
+   --service-account=projects/$PROJECT_ID/serviceAccounts/$PROJECT_NUMBER-compute@developer.gserviceaccount.com  \
+   --region=$REGION \
+   --branch-pattern='master'
+
+git checkout master
+git merge new-feature-1
+git push gcp master
+
+
+CANARY_URL=$(gcloud run services describe hello-cloudrun --platform managed --region $REGION --format=json | jq --raw-output ".status.traffic[] | select (.tag==\"canary\")|.url")
+echo $CANARY_URL
+
+curl -H "Authorization: Bearer $(gcloud auth print-identity-token)" $CANARY_URL
+
+gcloud builds triggers create github --name="tag" \
+   --repository=projects/$PROJECT_ID/locations/$REGION/connections/cloud-build-connection/repositories/cloudrun-progression \
+   --build-config='tag-cloudbuild.yaml' \
+   --service-account=projects/$PROJECT_ID/serviceAccounts/$PROJECT_NUMBER-compute@developer.gserviceaccount.com  \
+   --region=$REGION \
+   --tag-pattern='.*'
+
+git tag 1.1
+git push gcp 1.1
