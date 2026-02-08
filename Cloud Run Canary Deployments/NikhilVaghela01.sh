dce16867c64f26666e6c5a2c7aef37064e11afad
NikhilVaghela0716 <vaghelanikhil0716@gmail.com>
2026-02-08 12:47:15 +0530

    Create NikhilVaghela01.sh

---
+++ Cloud Run Canary Deployments/NikhilVaghela01.sh
@@ -0,0 +1,56 @@
+
+# ====================================================
+# COLOR & FORMATTING DEFINITIONS
+# ====================================================
+RED=$(tput setaf 1)
+BLUE=$(tput setaf 4)
+BOLD=$(tput bold)
+RESET=$(tput sgr0)
+
+# ====================================================
+# HEADER
+# ====================================================
+clear
+echo "${BLUE}${BOLD}==================================================================${RESET}"
+echo "${BLUE}${BOLD}           GOOGLE CLOUD REST API LAB | EXECUTION STARTED          ${RESET}"
+echo "${BLUE}${BOLD}==================================================================${RESET}"
+echo
+
+export PROJECT_ID=$(gcloud config get-value project)
+export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format='value(projectNumber)')
+REGION=$(gcloud compute project-info describe \
+  --format="value(commonInstanceMetadata.items[google-compute-default-region])")
+gcloud config set compute/region $REGION
+
+
+gcloud services enable \
+cloudresourcemanager.googleapis.com \
+container.googleapis.com \
+cloudbuild.googleapis.com \
+containerregistry.googleapis.com \
+run.googleapis.com \
+secretmanager.googleapis.com
+
+sleep 60
+
+
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member=serviceAccount:service-$PROJECT_NUMBER@gcp-sa-cloudbuild.iam.gserviceaccount.com \
+--role=roles/secretmanager.admin
+
+
+curl -sS https://webi.sh/gh | sh
+gh auth login
+gh api user -q ".login"
+GITHUB_USERNAME=$(gh api user -q ".login")
+git config --global user.name "${GITHUB_USERNAME}"
+git config --global user.email "${USER_EMAIL}"
+echo ${GITHUB_USERNAME}
+echo ${USER_EMAIL}
+
+
+echo
+echo "${RED_TEXT}${BOLD_TEXT}==============================================================${RESET_FORMAT}"
+echo "${RED_TEXT}${BOLD_TEXT} Follow the video and don’t run commands without watching it. ${RESET_FORMAT}"
+echo "${RED_TEXT}${BOLD_TEXT}==============================================================${RESET_FORMAT}"
+echo
