eca9622af365af50f2a3df5045cb2284c9d339ad
NikhilVaghela0716 <vaghelanikhil0716@gmail.com>
2026-02-24 17:29:28 +0530

    Update KenilithCloudx.sh

--- Rate Limiting with Cloud Armor/KenilithCloudx.sh
+++ Rate Limiting with Cloud Armor/KenilithCloudx.sh
@@ -20,47 +20,47 @@ read -p "ENTER ZONE_3: " ZONE3
 export REGION3="${ZONE3%-*}"
 
 echo
-echo "${BLUE_TEXT}${BOLD_TEXT}🔧 Fetching your GCP Project ID and Project Number...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Fetching your GCP Project ID and Project Number.${RESET_FORMAT}"
 PROJECT_ID=`gcloud config get-value project`
 export PROJECT_NUMBER=$(gcloud projects describe $PROJECT_ID --format="value(projectNumber)")
 echo "${BLUE_TEXT}Project ID set to: ${BOLD_TEXT}$PROJECT_ID${RESET_FORMAT}"
 echo "${BLUE_TEXT}Project Number set to: ${BOLD_TEXT}$PROJECT_NUMBER${RESET_FORMAT}"
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🛠️ Enabling the OS Config API for your project...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Enabling the OS Config API for your project.${RESET_FORMAT}"
 gcloud services enable osconfig.googleapis.com
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🛡️ Creating firewall rule to allow HTTP traffic...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating firewall rule to allow HTTP traffic...${RESET_FORMAT}"
 gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-http --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=0.0.0.0/0 --target-tags=http-server
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🛡️ Creating firewall rule for health checks...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating firewall rule for health checks...${RESET_FORMAT}"
 gcloud compute --project=$PROJECT_ID firewall-rules create default-allow-health-check --direction=INGRESS --priority=1000 --network=default --action=ALLOW --rules=tcp:80 --source-ranges=130.211.0.0/22,35.191.0.0/16 --target-tags=http-server
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}📄 Creating instance template for REGION1: ${REGION1}...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating instance template for REGION1: ${REGION1}...${RESET_FORMAT}"
 gcloud compute instance-templates create $REGION1-template --project=$PROJECT_ID --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script-url=gs://cloud-training/gcpnet/httplb/startup.sh,enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --region=$REGION1 --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=$REGION1-template,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250311,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}📄 Creating instance template for REGION2: ${REGION2}...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating instance template for REGION2: ${REGION2}...${RESET_FORMAT}"
 gcloud compute instance-templates create $REGION2-template --project=$PROJECT_ID --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=startup-script-url=gs://cloud-training/gcpnet/httplb/startup.sh,enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --region=$REGION2 --tags=http-server --create-disk=auto-delete=yes,boot=yes,device-name=$REGION2-template,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250311,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --reservation-affinity=any
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🏗️ Creating Managed Instance Group (MIG) and configuring autoscaling for REGION1: ${REGION1}...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating Managed Instance Group (MIG) and configuring autoscaling for REGION1: ${REGION1}${RESET_FORMAT}"
 gcloud beta compute instance-groups managed create $REGION1-mig --project=$PROJECT_ID --base-instance-name=$REGION1-mig --template=projects/$PROJECT_ID/global/instanceTemplates/$REGION1-template --size=1 --region=$REGION1 --target-distribution-shape=EVEN --instance-redistribution-type=proactive --default-action-on-vm-failure=repair --no-force-update-on-repair --standby-policy-mode=manual --list-managed-instances-results=pageless && gcloud beta compute instance-groups managed set-autoscaling $REGION1-mig --project=$PROJECT_ID --region=$REGION1 --mode=on --min-num-replicas=1 --max-num-replicas=5 --target-cpu-utilization=0.8 --cpu-utilization-predictive-method=none --cool-down-period=45
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🏗️ Creating Managed Instance Group (MIG) and configuring autoscaling for REGION2: ${REGION2}...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating Managed Instance Group (MIG) and configuring autoscaling for REGION2: ${REGION2}${RESET_FORMAT}"
 gcloud beta compute instance-groups managed create $REGION2-mig --project=$PROJECT_ID --base-instance-name=$REGION2-mig --template=projects/$PROJECT_ID/global/instanceTemplates/$REGION2-template --size=1 --region=$REGION2 --target-distribution-shape=EVEN --instance-redistribution-type=proactive --default-action-on-vm-failure=repair --no-force-update-on-repair --standby-policy-mode=manual --list-managed-instances-results=pageless && gcloud beta compute instance-groups managed set-autoscaling $REGION2-mig --project=$PROJECT_ID --region=$REGION2 --mode=on --min-num-replicas=1 --max-num-replicas=5 --target-cpu-utilization=0.8 --cpu-utilization-predictive-method=none --cool-down-period=45
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🔑 Setting up authentication token and project ID for API calls...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Setting up authentication token and project ID for API calls${RESET_FORMAT}"
 token=$(gcloud auth application-default print-access-token)
 project_id=$(gcloud config get-value project)
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🩺 Creating a global HTTP health check via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating a global HTTP health check via API${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{
@@ -76,14 +76,14 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/global/healthChecks"
 echo
-echo "${RED_TEXT}Waiting for health check to be provisioned... ⏳${RESET_FORMAT}"
+echo "${RED_TEXT}Waiting for health check to be provisioned.${RESET_FORMAT}"
 for i in $(seq 30 -1 1); do
   echo -ne "${RED_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
   sleep 1
 done
 echo -e "\n"
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🛡️ Creating a default security policy for the backend service via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating a default security policy for the backend service via API.${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{
@@ -106,14 +106,14 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$project_id/global/securityPolicies"
 echo
-echo "${RED_TEXT}Waiting for security policy to be provisioned... ⏳${RESET_FORMAT}"
+echo "${RED_TEXT}Waiting for security policy to be provisioned.${RESET_FORMAT}"
 for i in $(seq 30 -1 1); do
   echo -ne "${RED_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
   sleep 1
 done
 echo -e "\n"
 
-echo "${BLUE_TEXT}${BOLD_TEXT}⚙️ Creating the global backend service via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating the global backend service via API${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{
@@ -133,14 +133,14 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/global/backendServices"
 echo
-echo "${RED_TEXT}Waiting for backend service to be provisioned... ⏳${RESET_FORMAT}"
+echo "${RED_TEXT}Waiting for backend service to be provisioned${RESET_FORMAT}"
 for i in $(seq 60 -1 1); do
   echo -ne "${RED_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
   sleep 1
 done
 echo -e "\n"
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🗺️ Creating the URL map for the load balancer via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating the URL map for the load balancer via API${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{
@@ -149,14 +149,14 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$project_id/global/urlMaps"
 echo
-echo "${RED_TEXT}Waiting for URL map to be provisioned... ⏳${RESET_FORMAT}"
+echo "${RED_TEXT}Waiting for URL map to be provisioned${RESET_FORMAT}"
 for i in $(seq 30 -1 1); do
   echo -ne "${RED_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
   sleep 1
 done
 echo -e "\n"
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🎯 Creating the first Target HTTP Proxy via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating the first Target HTTP Proxy via API.${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{
@@ -166,7 +166,7 @@ curl -X POST -H "Content-Type: application/json" \
   "https://compute.googleapis.com/compute/v1/projects/$project_id/global/targetHttpProxies"
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🎯 Creating the second Target HTTP Proxy via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating the second Target HTTP Proxy via API${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{
@@ -175,14 +175,14 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/v1/projects/$project_id/global/targetHttpProxies"
 echo
-echo "${RED_TEXT}Waiting for target proxies to be provisioned... ⏳${RESET_FORMAT}"
+echo "${RED_TEXT}Waiting for target proxies to be provisioned${RESET_FORMAT}"
 for i in $(seq 30 -1 1); do
   echo -ne "${RED_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
   sleep 1
 done
 echo -e "\n"
 
-echo "${BLUE_TEXT}${BOLD_TEXT}➡️ Creating the IPv4 Global Forwarding Rule via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating the IPv4 Global Forwarding Rule via API${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{
@@ -196,14 +196,14 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/global/forwardingRules"
 echo
-echo "${RED_TEXT}Waiting for forwarding rule to be provisioned... ⏳${RESET_FORMAT}"
+echo "${RED_TEXT}Waiting for forwarding rule to be provisioned${RESET_FORMAT}"
 for i in $(seq 20 -1 1); do
   echo -ne "${RED_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
   sleep 1
 done
 echo -e "\n"
 
-echo "${BLUE_TEXT}${BOLD_TEXT}➡️ Creating the IPv6 Global Forwarding Rule via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating the IPv6 Global Forwarding Rule via API${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{
@@ -217,43 +217,43 @@ curl -X POST -H "Content-Type: application/json" \
   }' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/global/forwardingRules"
 echo
-echo "${RED_TEXT}Waiting for forwarding rule to be provisioned... ⏳${RESET_FORMAT}"
+echo "${RED_TEXT}Waiting for forwarding rule to be provisioned${RESET_FORMAT}"
 for i in $(seq 20 -1 1); do
   echo -ne "${RED_TEXT}  $i seconds remaining... \r${RESET_FORMAT}"
   sleep 1
 done
 echo -e "\n"
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🏷️ Setting named ports for the instance group in REGION1: ${REGION1} via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Setting named ports for the instance group in REGION1: ${REGION1} ${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{"namedPorts": [{"name": "http", "port": 80}]}' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/regions/$REGION1/instanceGroups/$REGION1-mig/setNamedPorts"
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🏷️ Setting named ports for the instance group in REGION2: ${REGION2} via API...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Setting named ports for the instance group in REGION2: ${REGION2} via API${RESET_FORMAT}"
 curl -X POST -H "Content-Type: application/json" \
   -H "Authorization: Bearer $token" \
   -d '{"namedPorts": [{"name": "http", "port": 80}]}' \
   "https://compute.googleapis.com/compute/beta/projects/$project_id/regions/$REGION2/instanceGroups/$REGION2-mig/setNamedPorts"
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🚀 Creating the 'siege-vm' instance in ZONE3: ${ZONE3}...${RESET_FORMAT}"
-echo "${RED_TEXT}${BOLD_TEXT}This will also configure Ops Agent and snapshot policies. ⚙️${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating the 'siege-vm' instance in ZONE3: ${ZONE3}${RESET_FORMAT}"
+echo "${RED_TEXT}${BOLD_TEXT}This will also configure Ops Agent and snapshot policies.${RESET_FORMAT}"
 gcloud compute instances create siege-vm --project=$PROJECT_ID --zone=$ZONE3 --machine-type=e2-medium --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default --metadata=enable-osconfig=TRUE,enable-oslogin=true --maintenance-policy=MIGRATE --provisioning-model=STANDARD --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append --create-disk=auto-delete=yes,boot=yes,device-name=siege-vm,image=projects/debian-cloud/global/images/debian-12-bookworm-v20250311,mode=rw,size=10,type=pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ops-agent-policy=v2-x86-template-1-4-0,goog-ec-src=vm_add-gcloud --reservation-affinity=any && printf 'agentsRule:\n  packageState: installed\n  version: latest\ninstanceFilter:\n  inclusionLabels:\n  - labels:\n      goog-ops-agent-policy: v2-x86-template-1-4-0\n' > config.yaml && gcloud compute instances ops-agents policies create goog-ops-agent-v2-x86-template-1-4-0-$ZONE3 --project=$PROJECT_ID --zone=$ZONE3 --file=config.yaml && gcloud compute resource-policies create snapshot-schedule default-schedule-1 --project=$PROJECT_ID --region=$REGION3 --max-retention-days=14 --on-source-disk-delete=keep-auto-snapshots --daily-schedule --start-time=16:00 && gcloud compute disks add-resource-policies siege-vm --project=$PROJECT_ID --zone=$ZONE3 --resource-policies=projects/$PROJECT_ID/regions/$REGION3/resourcePolicies/default-schedule-1
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}📦 Installing 'siege' utility on 'siege-vm' via SSH...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Installing 'siege' utility on 'siege-vm' via SSH.${RESET_FORMAT}"
 gcloud compute ssh --zone "$ZONE3" "siege-vm" --project "$PROJECT_ID" --command "sudo apt-get -y install siege" --quiet
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🛡️ Creating a new security policy 'rate-limit-siege' for rate limiting...${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Creating a new security policy 'rate-limit-siege' for rate limiting...${RESET_FORMAT}"
 gcloud compute security-policies create rate-limit-siege \
     --description "policy for rate limiting"
 echo
 
-echo "${BLUE_TEXT}${BOLD_TEXT}🚦 Adding a rate limiting rule to 'rate-limit-siege' policy...${RESET_FORMAT}"
-echo "${RED_TEXT}${BOLD_TEXT}This rule will ban IPs exceeding 50 requests in 120 seconds for 300 seconds. ✋${RESET_FORMAT}"
+echo "${BLUE_TEXT}${BOLD_TEXT}Adding a rate limiting rule to 'rate-limit-siege' policy${RESET_FORMAT}"
+echo "${RED_TEXT}${BOLD_TEXT}This rule will ban IPs exceeding 50 requests in 120 seconds for 300 seconds.${RESET_FORMAT}"
 gcloud beta compute security-policies rules create 100 \
     --security-policy=rate-limit-siege      \
     --expression="true" \
@@ -266,7 +266,7 @@ gcloud beta compute security-policies rules create 100 \
     --enforce-on-key=IP
 echo
 
-echo "${RED_TEXT}${BOLD_TEXT}🔗 Attaching the 'rate-limit-siege' security policy to the 'http-backend' service...${RESET_FORMAT}"
+echo "${RED_TEXT}${BOLD_TEXT}Attaching the 'rate-limit-siege' security policy to the 'http-backend' service${RESET_FORMAT}"
 gcloud compute backend-services update http-backend \
     --security-policy rate-limit-siege --global
 echo
