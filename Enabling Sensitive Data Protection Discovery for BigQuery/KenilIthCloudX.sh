2b20c3f01b4b3c51a69268bbabebc884d14d561e
NikhilVaghela0716 <vaghelanikhil0716@gmail.com>
2026-07-26 11:29:38 +0530

    Create KenilIthCloudX.sh

---
+++ Enabling Sensitive Data Protection Discovery for BigQuery/KenilIthCloudX.sh
@@ -0,0 +1,698 @@
+#!/bin/bash
+
+# Step 1: Get TEMPLATE_ID
+echo "${BLUE}${BOLD}Fetching TEMPLATE_ID from Google Cloud DLP API${RESET}"
+export TEMPLATE_ID=$(curl -s \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json" \
+"https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/locations/global/inspectTemplates" | jq -r '.inspectTemplates[0].name')
+
+# Step 2: Create an inspection template JSON file
+echo "${MAGENTA}${BOLD}Creating inspection_template.json${RESET}"
+cat <<EOF > inspection_template.json
+{
+  "inspectTemplate": {
+    "displayName": "Inspection Template for US SSN",
+    "description": "This template was created as part of a Sensitive Data Protection profiler configuration and was modified for deeper inspection for US Social Security numbers.",
+    "inspectConfig": {
+      "infoTypes": [
+        {
+          "name": "US_SOCIAL_SECURITY_NUMBER"
+        }
+      ],
+      "minLikelihood": "UNLIKELY"
+    }
+  }
+}
+EOF
+
+# Step 3: Updating the inspection template
+echo "${CYAN}${BOLD}Updating the inspection template using Google Cloud API${RESET}"
+curl -X PATCH -s \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json" \
+-d @inspection_template.json \
+"https://dlp.googleapis.com/v2/$TEMPLATE_ID"
+
+# Step 4: Create a de-identification template JSON file
+echo "${YELLOW}${BOLD}Creating deidentify-template.json${RESET}"
+cat <<EOF > deidentify-template.json
+{
+  "deidentifyTemplate": {
+    "deidentifyConfig": {
+      "recordTransformations": {
+        "fieldTransformations": [
+          {
+            "fields": [
+              {
+                "name": "ssn"
+              },
+              {
+                "name": "email"
+              }
+            ],
+            "primitiveTransformation": {
+              "replaceConfig": {
+                "newValue": {
+                  "stringValue": "[redacted]"
+                }
+              }
+            }
+          },
+          {
+            "fields": [
+              {
+                "name": "message"
+              }
+            ],
+            "infoTypeTransformations": {
+              "transformations": [
+                {
+                  "primitiveTransformation": {
+                    "replaceWithInfoTypeConfig": {}
+                  }
+                }
+              ]
+            }
+          }
+        ]
+      }
+    },
+    "displayName": "De-identification Template for US SSN"
+  },
+  "locationId": "global",
+  "templateId": "us_ssn_deidentify"
+}
+EOF
+
+# Step 5: Create the de-identification template in Google Cloud
+echo "${RED}${BOLD}Creating the de-identification template using Google Cloud API${RESET}"
+curl -X POST -s \
+-H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+-H "Content-Type: application/json" \
+-d @deidentify-template.json \
+"https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates"
+
+# Step 6: Creating the job configuration JSON file
+echo "${GREEN}${BOLD}Creating job-configuration.json${RESET}"
+cat > job-configuration.json << EOM
+{
+  "jobId": "us_ssn_inspection",
+  "inspectJob": {
+    "actions": [
+      {
+        "saveFindings": {
+          "outputConfig": {
+            "table": {
+              "projectId": "$DEVSHELL_PROJECT_ID",
+              "datasetId": "cloudstorage_inspection",
+              "tableId": "us_ssn"
+            }
+          }
+        }
+      },
+      {
+        "publishSummaryToCscc": {}
+      }
+    ],
+    "inspectConfig": {
+      "infoTypes": [
+        {
+          "name": "ADVERTISING_ID"
+        },
+        {
+          "name": "AGE"
+        },
+        {
+          "name": "ARGENTINA_DNI_NUMBER"
+        },
+        {
+          "name": "AUSTRALIA_TAX_FILE_NUMBER"
+        },
+        {
+          "name": "BELGIUM_NATIONAL_ID_CARD_NUMBER"
+        },
+        {
+          "name": "BRAZIL_CPF_NUMBER"
+        },
+        {
+          "name": "CANADA_SOCIAL_INSURANCE_NUMBER"
+        },
+        {
+          "name": "CHILE_CDI_NUMBER"
+        },
+        {
+          "name": "CHINA_RESIDENT_ID_NUMBER"
+        },
+        {
+          "name": "COLOMBIA_CDC_NUMBER"
+        },
+        {
+          "name": "CREDIT_CARD_NUMBER"
+        },
+        {
+          "name": "CREDIT_CARD_TRACK_NUMBER"
+        },
+        {
+          "name": "DATE_OF_BIRTH"
+        },
+        {
+          "name": "DENMARK_CPR_NUMBER"
+        },
+        {
+          "name": "EMAIL_ADDRESS"
+        },
+        {
+          "name": "ETHNIC_GROUP"
+        },
+        {
+          "name": "FDA_CODE"
+        },
+        {
+          "name": "FINLAND_NATIONAL_ID_NUMBER"
+        },
+        {
+          "name": "FRANCE_CNI"
+        },
+        {
+          "name": "FRANCE_NIR"
+        },
+        {
+          "name": "FRANCE_TAX_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "GENDER"
+        },
+        {
+          "name": "GERMANY_IDENTITY_CARD_NUMBER"
+        },
+        {
+          "name": "GERMANY_TAXPAYER_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "HONG_KONG_ID_NUMBER"
+        },
+        {
+          "name": "IBAN_CODE"
+        },
+        {
+          "name": "IMEI_HARDWARE_ID"
+        },
+        {
+          "name": "INDIA_AADHAAR_INDIVIDUAL"
+        },
+        {
+          "name": "INDIA_GST_INDIVIDUAL"
+        },
+        {
+          "name": "INDIA_PAN_INDIVIDUAL"
+        },
+        {
+          "name": "INDONESIA_NIK_NUMBER"
+        },
+        {
+          "name": "IRELAND_PPSN"
+        },
+        {
+          "name": "ISRAEL_IDENTITY_CARD_NUMBER"
+        },
+        {
+          "name": "JAPAN_INDIVIDUAL_NUMBER"
+        },
+        {
+          "name": "KOREA_RRN"
+        },
+        {
+          "name": "MAC_ADDRESS"
+        },
+        {
+          "name": "MEXICO_CURP_NUMBER"
+        },
+        {
+          "name": "NETHERLANDS_BSN_NUMBER"
+        },
+        {
+          "name": "NORWAY_NI_NUMBER"
+        },
+        {
+          "name": "PARAGUAY_CIC_NUMBER"
+        },
+        {
+          "name": "PASSPORT"
+        },
+        {
+          "name": "PERSON_NAME"
+        },
+        {
+          "name": "PERU_DNI_NUMBER"
+        },
+        {
+          "name": "PHONE_NUMBER"
+        },
+        {
+          "name": "POLAND_NATIONAL_ID_NUMBER"
+        },
+        {
+          "name": "PORTUGAL_CDC_NUMBER"
+        },
+        {
+          "name": "SCOTLAND_COMMUNITY_HEALTH_INDEX_NUMBER"
+        },
+        {
+          "name": "SINGAPORE_NATIONAL_REGISTRATION_ID_NUMBER"
+        },
+        {
+          "name": "SPAIN_CIF_NUMBER"
+        },
+        {
+          "name": "SPAIN_DNI_NUMBER"
+        },
+        {
+          "name": "SPAIN_NIE_NUMBER"
+        },
+        {
+          "name": "SPAIN_NIF_NUMBER"
+        },
+        {
+          "name": "SPAIN_SOCIAL_SECURITY_NUMBER"
+        },
+        {
+          "name": "STORAGE_SIGNED_URL"
+        },
+        {
+          "name": "STREET_ADDRESS"
+        },
+        {
+          "name": "SWEDEN_NATIONAL_ID_NUMBER"
+        },
+        {
+          "name": "SWIFT_CODE"
+        },
+        {
+          "name": "THAILAND_NATIONAL_ID_NUMBER"
+        },
+        {
+          "name": "TURKEY_ID_NUMBER"
+        },
+        {
+          "name": "UK_NATIONAL_HEALTH_SERVICE_NUMBER"
+        },
+        {
+          "name": "UK_NATIONAL_INSURANCE_NUMBER"
+        },
+        {
+          "name": "UK_TAXPAYER_REFERENCE"
+        },
+        {
+          "name": "URUGUAY_CDI_NUMBER"
+        },
+        {
+          "name": "US_BANK_ROUTING_MICR"
+        },
+        {
+          "name": "US_EMPLOYER_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "US_HEALTHCARE_NPI"
+        },
+        {
+          "name": "US_INDIVIDUAL_TAXPAYER_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "US_SOCIAL_SECURITY_NUMBER"
+        },
+        {
+          "name": "VEHICLE_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "VENEZUELA_CDI_NUMBER"
+        },
+        {
+          "name": "WEAK_PASSWORD_HASH"
+        },
+        {
+          "name": "AUTH_TOKEN"
+        },
+        {
+          "name": "AWS_CREDENTIALS"
+        },
+        {
+          "name": "AZURE_AUTH_TOKEN"
+        },
+        {
+          "name": "BASIC_AUTH_HEADER"
+        },
+        {
+          "name": "ENCRYPTION_KEY"
+        },
+        {
+          "name": "GCP_API_KEY"
+        },
+        {
+          "name": "GCP_CREDENTIALS"
+        },
+        {
+          "name": "JSON_WEB_TOKEN"
+        },
+        {
+          "name": "HTTP_COOKIE"
+        },
+        {
+          "name": "XSRF_TOKEN"
+        }
+      ],
+      "minLikelihood": "POSSIBLE"
+    },
+    "inspectTemplateName": "$TEMPLATE_ID",
+    "storageConfig": {
+      "cloudStorageOptions": {
+        "filesLimitPercent": 100,
+        "fileTypes": [
+          "TEXT_FILE",
+          "CSV"
+        ],
+        "fileSet": {
+          "url": "gs://$DEVSHELL_PROJECT_ID-input/**"
+        }
+      }
+    }
+  }
+}
+EOM
+
+# Step 7: Start the inspection job
+echo "${BLUE}${BOLD}Submitting the inspection job to Google Cloud DLP API${RESET}"
+curl -s \
+  -X POST \
+  -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+  -H "Content-Type: application/json" \
+  "https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/dlpJobs" \
+  -d @job-configuration.json
+
+# Step 8: Create de-identification job configuration JSON file
+echo "${MAGENTA}${BOLD}Creating de-identification job configuration JSON file${RESET}"
+cat > job-configuration.json << EOM
+{
+  "jobId": "us_ssn_deidentify",
+  "inspectJob": {
+    "actions": [
+      {
+        "deidentify": {
+          "fileTypesToTransform": [
+            "TEXT_FILE",
+            "CSV"
+          ],
+          "transformationDetailsStorageConfig": {
+            "table": {
+              "projectId": "$DEVSHELL_PROJECT_ID",
+              "datasetId": "cloudstorage_transformations",
+              "tableId": "deidentify_ssn_csv"
+            }
+          },
+          "transformationConfig": {
+            "structuredDeidentifyTemplate": "projects/$DEVSHELL_PROJECT_ID/locations/global/deidentifyTemplates/us_ssn_deidentify"
+          },
+          "cloudStorageOutput": "gs://$DEVSHELL_PROJECT_ID-output"
+        }
+      }
+    ],
+    "inspectConfig": {
+      "infoTypes": [
+        {
+          "name": "ADVERTISING_ID"
+        },
+        {
+          "name": "AGE"
+        },
+        {
+          "name": "ARGENTINA_DNI_NUMBER"
+        },
+        {
+          "name": "AUSTRALIA_TAX_FILE_NUMBER"
+        },
+        {
+          "name": "BELGIUM_NATIONAL_ID_CARD_NUMBER"
+        },
+        {
+          "name": "BRAZIL_CPF_NUMBER"
+        },
+        {
+          "name": "CANADA_SOCIAL_INSURANCE_NUMBER"
+        },
+        {
+          "name": "CHILE_CDI_NUMBER"
+        },
+        {
+          "name": "CHINA_RESIDENT_ID_NUMBER"
+        },
+        {
+          "name": "COLOMBIA_CDC_NUMBER"
+        },
+        {
+          "name": "CREDIT_CARD_NUMBER"
+        },
+        {
+          "name": "CREDIT_CARD_TRACK_NUMBER"
+        },
+        {
+          "name": "DATE_OF_BIRTH"
+        },
+        {
+          "name": "DENMARK_CPR_NUMBER"
+        },
+        {
+          "name": "EMAIL_ADDRESS"
+        },
+        {
+          "name": "ETHNIC_GROUP"
+        },
+        {
+          "name": "FDA_CODE"
+        },
+        {
+          "name": "FINLAND_NATIONAL_ID_NUMBER"
+        },
+        {
+          "name": "FRANCE_CNI"
+        },
+        {
+          "name": "FRANCE_NIR"
+        },
+        {
+          "name": "FRANCE_TAX_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "GENDER"
+        },
+        {
+          "name": "GERMANY_IDENTITY_CARD_NUMBER"
+        },
+        {
+          "name": "GERMANY_TAXPAYER_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "HONG_KONG_ID_NUMBER"
+        },
+        {
+          "name": "IBAN_CODE"
+        },
+        {
+          "name": "IMEI_HARDWARE_ID"
+        },
+        {
+          "name": "INDIA_AADHAAR_INDIVIDUAL"
+        },
+        {
+          "name": "INDIA_GST_INDIVIDUAL"
+        },
+        {
+          "name": "INDIA_PAN_INDIVIDUAL"
+        },
+        {
+          "name": "INDONESIA_NIK_NUMBER"
+        },
+        {
+          "name": "IRELAND_PPSN"
+        },
+        {
+          "name": "ISRAEL_IDENTITY_CARD_NUMBER"
+        },
+        {
+          "name": "JAPAN_INDIVIDUAL_NUMBER"
+        },
+        {
+          "name": "KOREA_RRN"
+        },
+        {
+          "name": "MAC_ADDRESS"
+        },
+        {
+          "name": "MEXICO_CURP_NUMBER"
+        },
+        {
+          "name": "NETHERLANDS_BSN_NUMBER"
+        },
+        {
+          "name": "NORWAY_NI_NUMBER"
+        },
+        {
+          "name": "PARAGUAY_CIC_NUMBER"
+        },
+        {
+          "name": "PASSPORT"
+        },
+        {
+          "name": "PERSON_NAME"
+        },
+        {
+          "name": "PERU_DNI_NUMBER"
+        },
+        {
+          "name": "PHONE_NUMBER"
+        },
+        {
+          "name": "POLAND_NATIONAL_ID_NUMBER"
+        },
+        {
+          "name": "PORTUGAL_CDC_NUMBER"
+        },
+        {
+          "name": "SCOTLAND_COMMUNITY_HEALTH_INDEX_NUMBER"
+        },
+        {
+          "name": "SINGAPORE_NATIONAL_REGISTRATION_ID_NUMBER"
+        },
+        {
+          "name": "SPAIN_CIF_NUMBER"
+        },
+        {
+          "name": "SPAIN_DNI_NUMBER"
+        },
+        {
+          "name": "SPAIN_NIE_NUMBER"
+        },
+        {
+          "name": "SPAIN_NIF_NUMBER"
+        },
+        {
+          "name": "SPAIN_SOCIAL_SECURITY_NUMBER"
+        },
+        {
+          "name": "STORAGE_SIGNED_URL"
+        },
+        {
+          "name": "STREET_ADDRESS"
+        },
+        {
+          "name": "SWEDEN_NATIONAL_ID_NUMBER"
+        },
+        {
+          "name": "SWIFT_CODE"
+        },
+        {
+          "name": "THAILAND_NATIONAL_ID_NUMBER"
+        },
+        {
+          "name": "TURKEY_ID_NUMBER"
+        },
+        {
+          "name": "UK_NATIONAL_HEALTH_SERVICE_NUMBER"
+        },
+        {
+          "name": "UK_NATIONAL_INSURANCE_NUMBER"
+        },
+        {
+          "name": "UK_TAXPAYER_REFERENCE"
+        },
+        {
+          "name": "URUGUAY_CDI_NUMBER"
+        },
+        {
+          "name": "US_BANK_ROUTING_MICR"
+        },
+        {
+          "name": "US_EMPLOYER_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "US_HEALTHCARE_NPI"
+        },
+        {
+          "name": "US_INDIVIDUAL_TAXPAYER_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "US_SOCIAL_SECURITY_NUMBER"
+        },
+        {
+          "name": "VEHICLE_IDENTIFICATION_NUMBER"
+        },
+        {
+          "name": "VENEZUELA_CDI_NUMBER"
+        },
+        {
+          "name": "WEAK_PASSWORD_HASH"
+        },
+        {
+          "name": "AUTH_TOKEN"
+        },
+        {
+          "name": "AWS_CREDENTIALS"
+        },
+        {
+          "name": "AZURE_AUTH_TOKEN"
+        },
+        {
+          "name": "BASIC_AUTH_HEADER"
+        },
+        {
+          "name": "ENCRYPTION_KEY"
+        },
+        {
+          "name": "GCP_API_KEY"
+        },
+        {
+          "name": "GCP_CREDENTIALS"
+        },
+        {
+          "name": "JSON_WEB_TOKEN"
+        },
+        {
+          "name": "HTTP_COOKIE"
+        },
+        {
+          "name": "XSRF_TOKEN"
+        }
+      ],
+      "minLikelihood": "POSSIBLE"
+    },
+    "storageConfig": {
+      "cloudStorageOptions": {
+        "filesLimitPercent": 100,
+        "fileTypes": [
+          "TEXT_FILE",
+          "CSV"
+        ],
+        "fileSet": {
+          "regexFileSet": {
+            "bucketName": "$DEVSHELL_PROJECT_ID-input",
+            "includeRegex": [],
+            "excludeRegex": [
+              "ignore"
+            ]
+          }
+        }
+      }
+    }
+  }
+}
+EOM
+
+# Step 9: Submit the de-identification job
+echo "${CYAN}${BOLD}Submitting the de-identification job to Google Cloud DLP API${RESET}"
+curl -s \
+  -X POST \
+  -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" \
+  -H "Content-Type: application/json" \
+  "https://dlp.googleapis.com/v2/projects/$DEVSHELL_PROJECT_ID/dlpJobs" \
+  -d @job-configuration.json
+
+
